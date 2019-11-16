4a057ec76bb3030f7fc07ce8cf62c118bec1a4eb
sebastian-geissler <sebastian.geissler@digitalwerk.net>
2019-11-16 14:10:13 +0100

    Add first version of OpenCV Filter

--- conan/opencv/conanfile.py
+++ conan/opencv/conanfile.py
@@ -1,32 +1,22 @@
 from conans import ConanFile, tools, CMake
 import os
 
-class ADTF3VideoToolbox(ConanFile):
-    name = "ADTF3VideoToolbox"
-    version = "0.1.0"
-    
+class OpenCV(ConanFile):
+    version = "4.1.0"
+    name = "opencv"
     settings = "os", "compiler", "build_type", "arch"
     description = "%s" % (name)
     generators = "cmake", "txt", "virtualenv"
     short_paths = True
-    keep_imports = True
-    no_copy_source = True
-    enable_multiconfig_package = False
-
-    def build_requirements(self):
-        self.build_requires("ADTF/3.6.2@dw/stable")
-        self.build_requires("OpenCV/4.1.0@dw/testing")
 
     def source(self):
-        self.run("git clone https://github.com/geisslersebastian/adtf3_video_toolbox.git")
+        self.run("git clone https://github.com/opencv/opencv")
 
     def build(self):
         cmake = CMake(self)
         # same as cmake.configure(source_folder=self.source_folder, build_folder=self.build_folder)
-        cmake.configure(source_folder="adtf3_video_toolbox")
+        cmake.configure(source_folder="opencv")
         cmake.build()
         #cmake.test() # Build the "RUN_TESTS" or "test" target
         # Build the "install" target, defining CMAKE_INSTALL_PREFIX to self.package_folder
         cmake.install()
-    
-
