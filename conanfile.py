baaa03fd4e1e59d4427a1415a1dac288dd48e71f
sebastian-geissler <sebastian.geissler@digitalwerk.net>
2019-11-18 20:54:25 +0100

    Small adjustments in conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 class ADTF3VideoToolbox(ConanFile):
     name = "ADTF3VideoToolbox"
     version = "0.1.0"
-    
+
     settings = "os", "compiler", "build_type", "arch"
     description = "%s" % (name)
     generators = "cmake", "txt", "virtualenv"
@@ -13,20 +13,22 @@ class ADTF3VideoToolbox(ConanFile):
     no_copy_source = True
     enable_multiconfig_package = False
 
+    scm = {
+         "type": "git",  # Use "type": "svn", if local repo is managed using SVN
+         "subfolder": "source",
+         "url": "https://github.com/geisslersebastian/adtf3_video_toolbox.git",
+         "revision": "auto"
+    }
+
     def build_requirements(self):
         self.build_requires("ADTF/3.6.2@dw/stable")
         self.build_requires("OpenCV/4.1.0@dw/testing")
 
-    def source(self):
-        self.run("git clone https://github.com/geisslersebastian/adtf3_video_toolbox.git")
-
     def build(self):
         cmake = CMake(self)
         # same as cmake.configure(source_folder=self.source_folder, build_folder=self.build_folder)
-        cmake.configure(source_folder="adtf3_video_toolbox")
+        cmake.configure(source_folder="source")
         cmake.build()
         #cmake.test() # Build the "RUN_TESTS" or "test" target
         # Build the "install" target, defining CMAKE_INSTALL_PREFIX to self.package_folder
         cmake.install()
-    
-
